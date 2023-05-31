fb005452ca4b98f0b47cbd278fda83b1c678c6ba
Nick Garfield <nick@clockwork.xyz>
2023-05-31 21:32:41 +0000

    Implement update

--- programs/nightfury/src/instructions/mod.rs
+++ programs/nightfury/src/instructions/mod.rs
@@ -1,11 +1,13 @@
 pub mod initialize;
 pub mod revoke;
 pub mod switch;
+pub mod update;
 pub mod update_day_uri;
 pub mod update_night_uri;
 
 pub use initialize::*;
 pub use revoke::*;
 pub use switch::*;
+pub use update::*;
 pub use update_day_uri::*;
 pub use update_night_uri::*;
---
+++ programs/nightfury/src/instructions/update.rs
@@ -0,0 +1,68 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::Mint;
+use clockwork_sdk::{
+    cpi::{thread_update, ThreadUpdate},
+    state::{Thread, ThreadAccount, Trigger},
+    ThreadProgram,
+};
+
+use crate::state::NightFury;
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    pub authority: Signer<'info>,
+    pub mint: Box<Account<'info, Mint>>,
+    #[account(mut, has_one = mint, has_one = thread, has_one = authority)]
+    pub nightfury: Account<'info, NightFury>,
+    #[account(mut, address = thread.pubkey(), constraint = thread.authority.eq(&nightfury.key()))]
+    pub thread: Account<'info, Thread>,
+    #[account(address = clockwork_sdk::ID)]
+    pub thread_program: Program<'info, ThreadProgram>,
+    pub system_program: Program<'info, System>,
+}
+
+pub fn process_update(ctx: Context<Update>, schedule: String) -> Result<()> {
+    // Terminates the automation.
+    // This instruction should clean up any delegations placed on NFTs.
+    // Delegate metadata update authorization to nightfury account.
+    let nightfury = &ctx.accounts.nightfury;
+
+    // Update the thread.
+    thread_update(
+        CpiContext::new_with_signer(
+            ctx.accounts.thread_program.to_account_info(),
+            ThreadUpdate {
+                authority: ctx.accounts.nightfury.to_account_info(),
+                system_program: ctx.accounts.system_program.to_account_info(),
+                thread: ctx.accounts.thread.to_account_info(),
+            },
+            &[&[
+                b"nightfury".as_ref(),
+                ctx.accounts.mint.key().as_ref(),
+                ctx.accounts.authority.key().as_ref(),
+                &[nightfury.bump],
+            ]],
+        ),
+        clockwork_sdk::state::ThreadSettings {
+            fee: None,
+            instructions: None,
+            name: None,
+            rate_limit: None,
+            trigger: Some(Trigger::Cron {
+                schedule,
+                skippable: true,
+            }),
+        },
+    )?;
+
+    // thread_delete(CpiContext::new_with_signer(
+    //     ctx.accounts.thread_program.to_account_info(),
+    //     ThreadDelete {
+    //         authority: ctx.accounts.nightfury.to_account_info(),
+    //         close_to: ctx.accounts.authority.to_account_info(),
+    //         thread: ctx.accounts.thread.to_account_info(),
+    //     },
+    // ))?;
+
+    Ok(())
+}
